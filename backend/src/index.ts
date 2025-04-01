7c7cc635609abf9ed0252eed617eef4acd6f0fe2
artem <artem@192.168.1.132>
2025-04-01 16:36:38 +0300

    added eslint, prettier and husky

---
+++ backend/src/index.ts
@@ -0,0 +1,4 @@
+const x: string[] = ['a', 'b']
+const y: readonly string[] = ['a', 'b']
+
+console.info(x, y)
--- webapp/eslint.config.js
+++ webapp/eslint.config.js
@@ -1,29 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import baseConfig from '../eslint.config.js'
 import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default tseslint.config({
+  extends: [...baseConfig],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  }
-)
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+})
