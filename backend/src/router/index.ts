a12bac31c8d2b0a8a4c75b164b955ce9b392bf7c
artem <artem@192.168.1.132>
2025-04-06 21:42:27 +0300

    added auto generate index

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,9 +1,18 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getTaskTrpcRoute } from './getTask'
 import { getTasksTrpcRoute } from './getTasks'
+// @endindex
 
 export const trpcRouter = trpc.router({
-  getTasks: getTasksTrpcRoute,
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getTask: getTaskTrpcRoute,
+  getTasks: getTasksTrpcRoute,
+  // @endindex
 })
 export type TrpcRouter = typeof trpcRouter
+
+// TODO @index('./**/index.ts', f => `//${JSON.stringify(f)}`)
+//{"path":"./getTask/index","name":"index","ext":".ts"}
+//{"path":"./getTasks/index","name":"index","ext":".ts"}
+// @endindex
